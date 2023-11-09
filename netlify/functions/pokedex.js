da37e4a4f1014d16b1fcdcb9c24c33239ac5a617
Truong Quy Hai <haitruong.tech@gmail.com>
2023-11-09 22:10:05 +0700

    handler parameters

--- netlify/functions/pokedex.js
+++ netlify/functions/pokedex.js
@@ -1,12 +1,14 @@
 import fetch from "node-fetch";
 
-export const handler = async () => {
-  const POKE_API = "https://pokeapi.co/api/v2/pokedex/kanto";
+export const handler = async (event, context) => {
+  console.log({ event, context });
+  const eventBody = JSON.parse(event.body);
+  const POKE_API = `https://pokeapi.co/api/v2/pokedex/${eventBody.region}`;
 
   const data = await fetch(POKE_API).then((response) => response.json());
 
   return {
     statusCode: 200,
-    body: JSON.stringify({ data }),
+    body: JSON.stringify({ data: data.pokemon_entries }),
   };
 };
